cba42973c1c38a076680af2f994be75f841d6b57
Jay Potter <j@ypotter.ca>
2024-11-02 14:07:19 -0400

    Update DateTime.php

--- src/Potter/DateTime/DateTime.php
+++ src/Potter/DateTime/DateTime.php
@@ -4,5 +4,14 @@ declare(strict_types=1);
 
 namespace Potter\DateTime;
 
+use DateTimeZone;
+use Potter\DateTime\Zone\TimeZone;
+
 final class DateTime extends AbstractDateTime
-{ }
+{
+    public function getTimezone(): DateTimeZone|false
+    {
+        return ($timeZone = parent::getTimezone()) === false ?
+            false : TimeZone::inherit($timeZone);
+    }
+}
