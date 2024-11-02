22e0611e02273e00f71d29c28b5175f73c7eb6a2
Jay Potter <j@ypotter.ca>
2024-11-02 14:07:01 -0400

    Update AbstractDateTime.php

--- src/Potter/DateTime/AbstractDateTime.php
+++ src/Potter/DateTime/AbstractDateTime.php
@@ -4,14 +4,7 @@ declare(strict_types=1);
 
 namespace Potter\DateTime;
 
-use DateInterval, DateTime, DateTimeZone;
+use \DateTime as BaseDateTime;
 
-abstract class AbstractDateTime extends DateTime implements DateTimeInterface
-{
-    abstract public function diff(DateTimeInterface $targetObject, bool $absolute = false): DateInterval;
-    abstract public function format(string $format): string;
-    abstract public function getOffset(): int;
-    abstract public function getTimestamp(): int;
-    abstract public function getTimezone(): DateTimeZone|false;
-    abstract public function __wakeup(): void;
-}
+abstract class AbstractDateTime extends BaseDateTime implements DateTimeInterface
+{ }
